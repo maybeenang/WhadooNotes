74dabda954061b59cbbf41680036a5233f5bc32e
maybeenang <muhammad.120140194@student.itera.ac.id>
2022-12-13 02:58:59 +0700

    completed ui

--- src/screens/HomeNavigator/AddTodo.js
+++ src/screens/HomeNavigator/AddTodo.js
@@ -1,12 +1,77 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const AddTodo = () => {
   return (
-    <View>
-      <Text>AddTodo</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.button}>
+          <Ionicons name="arrow-back" size={24} color="#676767" />
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, { width: 80 }]}>
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: "500",
+              textAlign: "center",
+              color: "#676767",
+            }}
+          >
+            Save
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput placeholder="Untitled" style={styles.titleInput} />
+      <View style={{ flex: 1 }}>
+        <TextInput
+          placeholder="Type something..."
+          style={styles.input}
+          multiline={true}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default AddTodo;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 30,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#F2F2F2",
+    padding: 10,
+    borderRadius: 5,
+  },
+  titleInput: {
+    height: 50,
+    fontSize: 24,
+    fontWeight: "700",
+    color: "#676767",
+    marginTop: 20,
+    borderBottomWidth: 2,
+    borderBottomColor: "#F2F2F2",
+  },
+  input: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#676767",
+    marginTop: 20,
+  },
+});
